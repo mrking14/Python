7c98ab41001634348c8cff04a33112c0b6656d82
Masoom Raza <masoomraza843113@outlook.com>
2026-06-17 08:16:18 +0530

    Variable Example Addded

--- variable.py
+++ variable.py
@@ -1,7 +1,19 @@
-age = 25;
+#Intiger Example
+age = 25; 
+
+# Sring Example
 name = "Masoom raza";
 contact = "8581094226";
 
-print(age);
-print(name);
-print(contact);
+# float Example
+valueOfPi = 3.14;
+
+# Boolean Example
+availabe = True;
+
+
+print( "Your age is :", age);
+print("Your name is :", name);
+print("Your contact is :",contact);
+print("Value of Pi is :", valueOfPi);
+print("Are you avalable:", availabe);
