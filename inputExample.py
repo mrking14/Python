442643972a9d4d0711bb6bf024ff1a2455e47dcd
Masoom Raza <masoomraza843113@outlook.com>
2026-06-17 08:23:45 +0530

    Input Example added

---
+++ inputExample.py
@@ -0,0 +1,8 @@
+# Input Example
+name = input("Enter your Name: ");
+age = int(input("Enter your age: "));
+contact = input("Enter your Contact Number: ");
+
+print("Your name is ",name)
+print("Your age is ",age)
+print("Your contact is ",contact)
