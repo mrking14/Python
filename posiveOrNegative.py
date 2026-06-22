59a3132e72507b20b1c14b33a0eb3b3d33ed2c25
Masoom Raza <masoomraza843113@outlook.com>
2026-06-22 22:40:09 +0530

    Posive Negative number Example is added

---
+++ posiveOrNegative.py
@@ -0,0 +1,8 @@
+num = int(input("Enter number: "))
+
+if(num> 0):
+    print ("Given number is Positive!")
+elif(num <0):
+    print("Given number is Negative!")
+else:
+    print ("Given number is Zero!")
