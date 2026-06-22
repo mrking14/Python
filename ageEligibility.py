c2f151832dcb99c20e2fe0d3b837031641d6ca51
Masoom Raza <masoomraza843113@outlook.com>
2026-06-22 22:50:40 +0530

    Odd and Even  number Example is added

---
+++ ageEligibility.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age: "))
+
+if age>18:
+    print("You're Eligible!")
+else:
+    print("You're not Eligible")
---
+++ oddOrEven.py
@@ -0,0 +1,6 @@
+number = int(input("Enter number to check Odd Or Even:"))
+
+if number%2 ==0:
+    print("Given number is Even!")
+else:
+    print("Given Number is Odd")
