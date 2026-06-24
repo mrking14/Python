bedb1c2d87e5f36813a8c1974da84e2b432e1133
Masoom Raza <masoomraza843113@outlook.com>
2026-06-24 11:23:39 +0530

    Example of Break Pass and Continue Example Added

---
+++ loop/starPrint.py
@@ -0,0 +1,6 @@
+end = int(input("Enter Number to Print Star: "))
+num = 1
+
+while num<=end:
+    print("*"*num)
+    num += 1
---
+++ loop/table.py
@@ -0,0 +1,6 @@
+number = int(input("Enter number to Print Table|: "))
+start = 1
+
+while start <=10:
+    print(number," X ", start, " = ", number*start)
+    start+=1
