748390c81a0bb9437ebbcf74227e67ca05eced02
Masoom Raza <masoomraza843113@outlook.com>
2026-06-25 08:43:16 +0530

    String basic Example Added.

---
+++ stringExapmles/basicString.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+
+print("First character of your name is: ", name[0])
+print("Last Charactor of your name is:", name[len(name)-1])
+print("Length of Your Name is : ", len(name))
