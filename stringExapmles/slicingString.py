b0dbda5ed4fb9bcde9253c231495e3521a6d85a1
Masoom Raza <masoomraza843113@outlook.com>
2026-06-25 10:46:19 +0530

    Slicing String Example Added

---
+++ stringExapmles/slicingString.py
@@ -0,0 +1,4 @@
+text = "Python Programming"
+print("First 6 charactar of string is: ",text[:6])
+print("Last 6 Charactar of String is: ", text[-6:])
+print("Every Second charatar of String: ",text[::2])
